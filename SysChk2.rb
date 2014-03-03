4d18693f97512c61f3e8d442fc76f62343e9bdc5
Dan Pickett <dan.pickett@launchware.com>
2014-03-03 15:53:19 -0500

    adjust whitespace for readability and conventions

--- SysChk2.rb
+++ SysChk2.rb
@@ -19,67 +19,65 @@ end
 
 get '/leaderboard' do
 
-@teams =
-[
-  { team_name: "Patriots",
-    win_count: 0,
-    loss_count: 0
-  },
-  { team_name: "Broncos",
-    win_count: 0,
-    loss_count: 0
-  },
-  { team_name: "Steelers",
-    win_count: 0,
-    loss_count: 0
-  },
-  { team_name: "Colts",
-    win_count: 0,
-    loss_count: 0
-  },
-]
+  @teams = [
+    { team_name: "Patriots",
+      win_count: 0,
+      loss_count: 0
+    },
+    { team_name: "Broncos",
+      win_count: 0,
+      loss_count: 0
+    },
+    { team_name: "Steelers",
+      win_count: 0,
+      loss_count: 0
+    },
+    { team_name: "Colts",
+      win_count: 0,
+      loss_count: 0
+    },
+  ]
 
 
-    @scores =
-    [
-      {
-        home_team: "Patriots",
-        away_team: "Broncos",
-        home_score: 7,
-        away_score: 3
-      },
-      {
-        home_team: "Broncos",
-        away_team: "Colts",
-        home_score: 3,
-        away_score: 0
-      },
-      {
-        home_team: "Patriots",
-        away_team: "Colts",
-        home_score: 11,
-        away_score: 7
-      },
-      {
-        home_team: "Steelers",
-        away_team: "Patriots",
-        home_score: 7,
-        away_score: 21
-      }
-    ]
+  @scores = [
+    {
+      home_team: "Patriots",
+      away_team: "Broncos",
+      home_score: 7,
+      away_score: 3
+    },
+    {
+      home_team: "Broncos",
+      away_team: "Colts",
+      home_score: 3,
+      away_score: 0
+    },
+    {
+      home_team: "Patriots",
+      away_team: "Colts",
+      home_score: 11,
+      away_score: 7
+    },
+    {
+      home_team: "Steelers",
+      away_team: "Patriots",
+      home_score: 7,
+      away_score: 21
+    }
+  ]
 
-    @scores.each do |score|
-        score[:winner] = whowon(score)
-        score[:loser] = wholost(score)
-        @teams.each do |team|
-          if score[:winner] == team[:team_name]
-            team[:win_count]+=1
-          end
-          if score[:loser] == team[:team_name]
-            team[:loss_count]+=1
-          end
+  @scores.each do |score|
+      score[:winner] = whowon(score)
+      score[:loser] = wholost(score)
+      @teams.each do |team|
+        if score[:winner] == team[:team_name]
+          team[:win_count]+=1
         end
-    end
+        if score[:loser] == team[:team_name]
+          team[:loss_count]+=1
+        end
+      end
+  end
 
-    erb:index
+  erb:index
 end
